41f0b815e982e11877549ce6243125da0131c63d
rwcds <rwcds@rwcds>
2017-06-07 13:54:35 -0700

    modified to run on server (Leffler)

--- script/get_nwrfc_gridded.sh
+++ script/get_nwrfc_gridded.sh
@@ -33,8 +33,8 @@ URLBASE=https://www.nwrfc.noaa.gov/weather/netcdf/$YEAR/$DATE
 
 cd /usr/dx/nwdp/nwrfc_gridded/raw/
 rm *.nc
-wget $URLBASE/QTF.${DATE}12.nc.gz
-wget $URLBASE/QTE.${DATE}12.nc.gz
+#wget $URLBASE/QTF.${DATE}12.nc.gz
+#wget $URLBASE/QTE.${DATE}12.nc.gz
 wget $URLBASE/QPF.${DATE}12.nc.gz
 wget $URLBASE/QPE.${DATE}12.nc.gz
 
@@ -50,7 +50,7 @@ rm *.asc
 cd ../script
 . env/bin/activate
 ./resample.py ../raw/QPE.${DATE}12.nc ../raw/QPF.${DATE}12.nc
-./resample_tempair.py ../raw/QTE.${DATE}12.nc ../raw/QTF.${DATE}12.nc
+#./resample_tempair.py ../raw/QTE.${DATE}12.nc ../raw/QTF.${DATE}12.nc
 
 mv ../temp/*.dss ../data
 
