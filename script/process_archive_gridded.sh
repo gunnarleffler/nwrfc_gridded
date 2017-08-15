62fc7be7715310e4eeb5e27edc5eec239ef6cd1c
rwcds <rwcds@rwcds>
2017-08-15 14:12:55 -0700

    added script to process archived g

---
+++ script/process_archive_gridded.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# process_archive_gridded.sh
+#
+# converts to a .DSS file. 
+
+
+# Resample netCDF gridded data to dss
+#---------------------------------------------------
+. env/bin/activate
+
+cd /home/rwcds/dx/nwdp/nwrfc_gridded/archive
+
+for DATE in *; do
+  echo $DATE
+  cd ../script
+  pwd 
+  ./resample_tempair.py ../archive/${DATE}/QTE.${DATE}12.nc ../archive/${DATE}/QTF.${DATE}12.nc
+  ./resample.py ../archive/${DATE}/QPE.${DATE}12.nc ../archive/${DATE}/QPF.${DATE}12.nc
+
+  rm ../temp/*.asc
+
+done
+
+cp ../temp/*.dss ../data
