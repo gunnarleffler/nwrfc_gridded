7e64ab46b5e4b5e757d084d6dd9a1c5d68b8f7c7
rwcds <rwcds@rwcds>
2018-11-15 13:28:11 -0800

    updated basin extents

--- script/process_archive_gridded.sh
+++ script/process_archive_gridded.sh
@@ -13,13 +13,13 @@ cd $DX_HOME/nwdp/nwrfc_gridded/script
 
 DATELIST=`ls -1 ../archive | awk -F"." '{print $2}' | sort -u`
 
-cnt=9; 
+cnt=6; 
 for DATE in $DATELIST; do
   echo $DATE
   let cnt=cnt+1;
-  #Each netCDF file from the RFC has 10 days worth of data
-  #So we only need to process every 10th file
-  if [ $cnt -eq 10 ]; then
+  #Each netCDF file from the RFC has minimum 7 days worth of data
+  #So we only need to process every 7th file
+  if [ $cnt -eq 7 ]; then
     gunzip -v ../archive/*${DATE}.nc.gz
 
     ./resample_tempair.py ../archive/QTE.${DATE}.nc ../archive/QTF.${DATE}.nc
